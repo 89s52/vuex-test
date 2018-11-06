cc718cac46cbe33211b507f558b556d9bae5fd05
89s52 <25033402+89s52@users.noreply.github.com>
2018-11-06 10:56:38 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import getData from '@/components/getData'
+import sockData from '@/components/sockData'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/getData',
       name: 'getData',
       component: getData
+    },
+    {
+      path: '/sockData',
+      name: 'sockData',
+      component: sockData
     }
   ]
 })
