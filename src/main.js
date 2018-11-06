2c2f891eb0e66b46afcc2f83dd0065377c6c0414
89s52 <25033402+89s52@users.noreply.github.com>
2018-11-06 11:03:49 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
+import VueSocketio from 'vue-socket.io';
+import vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.use(vuex)
+Vue.use(VueSocketio, 'http://localhost:3000', store);
 
 /* eslint-disable no-new */
 new Vue({
