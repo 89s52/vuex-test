1fe5936e72c229f6086bfc9dc588ac551c89c57a
89s52 <25033402+89s52@users.noreply.github.com>
2018-11-06 11:01:16 +0800

    Update store.js

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getData from './modules/get'
+import getData from './modules/getData/get'
+import webSocket from './modules/webSocket-test/webSocket'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        getData: getData
+        getData: getData,
+        webSocket: webSocket
     },
 });
